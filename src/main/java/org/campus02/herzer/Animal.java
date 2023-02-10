2d953923f70c64913a84b43ba5a94c6f0164d2c9
rezreh <s51274.edu.campus02.at>
2023-02-10 12:08:02 +0100

    Added Methods and Tests

--- src/main/java/org/campus02/herzer/Animal.java
+++ src/main/java/org/campus02/herzer/Animal.java
@@ -2,7 +2,70 @@ package org.campus02.herzer;
 
 public class Animal {
 
-public void testMethod(){
+    private String name;
+    private String species;
+    private String loud;
+    private int age;
 
-}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Animal(String name, String species, String loud, int age) {
+        this.name = name;
+        this.species = species;
+        this.loud = loud;
+        setAge(age);
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getLoud() {
+        return loud;
+    }
+
+    public void setLoud(String loud) {
+        this.loud = loud;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<=0)
+        {
+            age=0;
+        }
+        this.age = age;
+    }
+
+    public String giveAnimalLoud() {
+        return this.loud;
+    }
+
+    public String getNameAndAge() {
+        return "Name: " + this.name + ", Age: " + this.age;
+    }
+
+    public String getAgeCategory() {
+        if (age > 4) {
+            return "old";
+        } else if (age > 2) {
+            return "young";
+        } else {
+            return "baby";
+        }
+
+    }
 }
